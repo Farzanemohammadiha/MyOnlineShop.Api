cffca989e1b984eb850fa0fd7a22c4edb5b9397d
Farzanehmohammadiha <farzaneh.mohammadi144@gmail.com>
2025-10-24 12:37:40 +0330

    feat(models): add Product and DiscountCoupon

---
+++ MyOnlineShop.Api/Models/DiscountCoupon.cs
@@ -0,0 +1,11 @@
+﻿namespace MyOnlineShop.Api.Models
+{
+    public class DiscountCoupon
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public int Percentage { get; set; }
+        public DateTime Expiry { get; set; }
+        public bool IsActive {  get; set; }
+    }
+}
---
+++ MyOnlineShop.Api/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿namespace MyOnlineShop.Api.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int? DiscountPercent { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+
+
+        public decimal GetPriceAfterProductDiscount()
+        {
+            if(DiscountPercent.HasValue && DiscountPercent.Value > 0)
+            {
+               return Math.Round(Price * (100 - DiscountPercent.Value) / 100M, 2);
+            }
+            return Price;
+        }
+    }
+}
